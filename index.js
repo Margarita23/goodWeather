781a9c58e50cc2aaf9a2b3273342e040bc9409b3
Marharita Andriukhina <margarita.andriukhina@gmail.com>
2020-03-13 12:27:24 +0200

    delete node_modules folder, add some styles to hello page and add content to hello page

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+// ***** settings express *****
 const express = require('express')
 const app = express();
 
+// app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + '/public'));
+
+const port = 8080;
+
+// ***** settings db *****
+var mysql = require('mysql');
+
+
 app.get('/hello', (req, res) => {
-  res.send({'message': 'hello world!'});
+  // res.send({'message': 'hello world!!!!!!'});
+  res.render("index", { title: "Home" });
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log('Start app listening on port 8080!')
 });
